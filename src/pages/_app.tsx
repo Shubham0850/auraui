486d26b5c6cbfd63677a87b7037f28846366074f
Jayjeet1013 <jayjeet1013@gmail.com>
2024-05-13 21:13:39 +0530

    FIX:remove footer & navbar f

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,9 +6,9 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Navbar />
+    
       <Component {...pageProps} />
-      <Footer />
+    
     </>
   );
 }
