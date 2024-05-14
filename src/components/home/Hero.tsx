f22920a1297c4f2a49f1621383f50e654fc656fd
Jayjeet1013 <jayjeet1013@gmail.com>
2024-05-14 18:24:29 +0530

    update

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -5,7 +5,7 @@ function Hero() {
   return (
     <div className="py-20  ">
       <Link href={"/introduction"}>
-        <button className="p-2  bg-black text-white ">Browse Components</button>
+        <button className="px-4 py-2 rounded-lg bg-black text-white ">Browse Components</button>
       </Link>
     </div>
   );
