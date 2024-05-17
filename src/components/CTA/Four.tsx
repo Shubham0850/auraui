6b8c6a54dee613db71bd56032b752bdf98f2c600
Kishor <yoyokunal156@gmail.com>
2024-05-17 17:48:45 +0530

    Cta, footer and contacts done

--- src/components/CTA/Four.tsx
+++ src/components/CTA/Four.tsx
@@ -1,49 +1,80 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const four = (props: Props) => {
-	return (
-		<section className="py-10 bg-gray-100 sm:py-16 lg:py-24">
-    <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+  return (
+    <section className="py-10 bg-gray-100 sm:py-16 lg:py-24">
+      <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
         <div className="max-w-2xl mx-auto text-center">
-            <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">Get full access to Celebration</h2>
-            <p className="max-w-xl mx-auto mt-4 text-base leading-relaxed text-gray-600">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+          <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">
+            Get full access to Celebration
+          </h2>
+          <p className="max-w-xl mx-auto mt-4 text-base leading-relaxed text-gray-600">
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim velit mollit. Exercitation
+            veniam consequat sunt nostrud amet.
+          </p>
         </div>
 
         <form action="#" method="POST" className="max-w-xl mx-auto mt-12">
-            <div className="flex flex-col items-center sm:flex-row sm:justify-center">
-                <div className="flex-1 w-full min-w-0 px-4 sm:px-0">
-                    <label htmlFor="email" className="sr-only"></label>
-                    <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        placeholder="Enter email to get started"
-                        className="block w-full px-4 py-4 text-base text-black placeholder-gray-500 transition-all duration-200 border-transparent rounded-md caret-indigo-600 focus:border-indigo-600 focus:ring-1 focus:ring-indigo-600"
-                        required
-                    />
-                </div>
-
-                <button type="submit" className="inline-flex items-center justify-center w-auto px-4 py-4 mt-4 font-semibold text-white transition-all duration-200 bg-indigo-600 border border-transparent rounded-md sm:ml-4 sm:mt-0 sm:w-auto hover:bg-indigo-700 focus:bg-indigo-700">
-                    Get instant access
-                    <svg className="w-5 h-5 ml-3 -mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd" />
-                    </svg>
-                </button>
+          <div className="flex flex-col items-center sm:flex-row sm:justify-center">
+            <div className="flex-1 w-full min-w-0 px-4 sm:px-0">
+              <label htmlFor="email" className="sr-only"></label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Enter email to get started"
+                className="block w-full px-4 py-4 text-base text-black placeholder-gray-500 transition-all duration-200 border-transparent rounded-md caret-indigo-600 focus:border-indigo-600 focus:ring-1 focus:ring-indigo-600"
+                required
+              />
             </div>
+
+            <button
+              type="submit"
+              className="inline-flex items-center justify-center w-auto px-4 py-4 mt-4 font-semibold text-white transition-all duration-200 bg-indigo-600 border border-transparent rounded-md sm:ml-4 sm:mt-0 sm:w-auto hover:bg-indigo-700 focus:bg-indigo-700"
+            >
+              Get instant access
+              <svg
+                className="w-5 h-5 ml-3 -mr-1"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
         </form>
 
         <div className="flex items-center justify-center px-8 mt-8 sm:px-0">
-            <svg className="flex-shrink-0 w-5 h-5 text-gray-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
-            </svg>
-            <span className="ml-2 text-sm text-gray-600"> Your data is complely secured with us. We don’t share with anyone. </span>
+          <svg
+            className="flex-shrink-0 w-5 h-5 text-gray-600"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="1.5"
+              d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
+            />
+          </svg>
+          <span className="ml-2 text-sm text-gray-600">
+            {" "}
+            Your data is complely secured with us. We don’t share with anyone.{" "}
+          </span>
         </div>
-    </div>
-</section>
+      </div>
+    </section>
+  );
+};
 
-	)
-}
-
-export default four
+export default four;
--- src/components/CTA/One.tsx
+++ src/components/CTA/One.tsx
@@ -1,40 +1,61 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const one = (props: Props) => {
-	return (
-		<section className="bg-blue-600 2xl:py-24 2xl:bg-white">
-    <div className="px-4 mx-auto overflow-hidden bg-blue-600 max-w-7xl sm:px-6 lg:px-8">
+  return (
+    <section className="bg-blue-600 2xl:py-24 2xl:bg-white">
+      <div className="px-4 mx-auto overflow-hidden bg-blue-600 max-w-7xl sm:px-6 lg:px-8">
         <div className="py-10 sm:py-16 lg:py-24 2xl:pl-24">
-            <div className="grid items-center grid-cols-1 gap-y-12 lg:grid-cols-2 lg:gap-x-8 2xl:gap-x-20">
-                <div>
-                    <h2 className="text-3xl font-bold leading-tight text-white sm:text-4xl lg:text-5xl lg:leading-tight">Use mobile app for better performance</h2>
-                    <p className="mt-4 text-base text-gray-50">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+          <div className="grid items-center grid-cols-1 gap-y-12 lg:grid-cols-2 lg:gap-x-8 2xl:gap-x-20">
+            <div>
+              <h2 className="text-3xl font-bold leading-tight text-white sm:text-4xl lg:text-5xl lg:leading-tight">
+                Use mobile app for better performance
+              </h2>
+              <p className="mt-4 text-base text-gray-50">
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                amet sint. Velit officia consequat duis enim velit mollit.
+                Exercitation veniam consequat sunt nostrud amet.
+              </p>
 
-                    <div className="flex flex-row items-center mt-8 space-x-4 lg:mt-12">
-                        <a href="#" title="" className="flex" role="button">
-                            <img className="w-auto h-14" src="https://cdn.rareblocks.xyz/collection/celebration/images/cta/8/btn-app-store.svg" alt="" />
-                        </a>
+              <div className="flex flex-row items-center mt-8 space-x-4 lg:mt-12">
+                <a href="#" title="" className="flex" role="button">
+                  <img
+                    className="w-auto h-14"
+                    src="https://cdn.rareblocks.xyz/collection/celebration/images/cta/8/btn-app-store.svg"
+                    alt=""
+                  />
+                </a>
 
-                        <a href="#" title="" className="flex" role="button">
-                            <img className="w-auto h-14" src="https://cdn.rareblocks.xyz/collection/celebration/images/cta/8/btn-play-store.svg" alt="" />
-                        </a>
-                    </div>
-                </div>
-
-                <div className="relative px-12">
-                    <svg className="absolute inset-x-0 bottom-0 left-1/2 -translate-x-1/2 -mb-48 lg:-mb-72 text-yellow-300 w-[460px] h-[460px] sm:w-[600px] sm:h-[600px]" fill="currentColor" viewBox="0 0 8 8">
-                        <circle cx="4" cy="4" r="3" />
-                    </svg>
-                    <img className="relative w-full max-w-xs mx-auto -mb-60 lg:-mb-64" src="https://cdn.rareblocks.xyz/collection/celebration/images/cta/8/iphone-mockup.png" alt="" />
-                </div>
+                <a href="#" title="" className="flex" role="button">
+                  <img
+                    className="w-auto h-14"
+                    src="https://cdn.rareblocks.xyz/collection/celebration/images/cta/8/btn-play-store.svg"
+                    alt=""
+                  />
+                </a>
+              </div>
             </div>
+
+            <div className="relative px-12">
+              <svg
+                className="absolute inset-x-0 bottom-0 left-1/2 -translate-x-1/2 -mb-48 lg:-mb-72 text-yellow-300 w-[460px] h-[460px] sm:w-[600px] sm:h-[600px]"
+                fill="currentColor"
+                viewBox="0 0 8 8"
+              >
+                <circle cx="4" cy="4" r="3" />
+              </svg>
+              <img
+                className="relative w-full max-w-xs mx-auto -mb-60 lg:-mb-64"
+                src="https://cdn.rareblocks.xyz/collection/celebration/images/cta/8/iphone-mockup.png"
+                alt=""
+              />
+            </div>
+          </div>
         </div>
-    </div>
-</section>
+      </div>
+    </section>
+  );
+};
 
-	)
-}
-
-export default one
+export default one;
