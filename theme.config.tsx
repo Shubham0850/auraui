fa0b2003d6e80cea2a7037ae36b4ff55f25b0353
Jayjeet1013 <jayjeet1013@gmail.com>
2024-05-18 13:29:27 +0530

    logo for dark mode updated

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,10 +1,23 @@
 import React from "react";
-import { DocsThemeConfig } from "nextra-theme-docs";
+import { DocsThemeConfig} from "nextra-theme-docs";
 import Footer from "@/components/layout/Footer";
+import { title } from "process";
+import { useTheme} from "nextra-theme-docs";
+
 
 const config: DocsThemeConfig = {
-  logo: <img src="/logo-light.png" className="h-10" alt="logo" />,
-
+  logo: () => {
+    const { theme } = useTheme();
+    return (
+      <>
+        {theme === "light" ? (
+          <img className="w-auto h-8" src="/logo-light.png" alt="Logo Light" />
+        ) : (
+          <img className="w-auto h-8" src="/logo-dark.png" alt="Logo Dark" />
+        )}
+      </>
+    );
+  },
   head: (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -13,8 +26,15 @@ const config: DocsThemeConfig = {
         property="og:description"
         content="AuraUI the best platform for website components "
       />
+      <link rel="icon" href="/logo.png" />
     </>
   ),
+
+//  navbar:{
+//    component:'darkMode',
+//    extraContent: ""
+//  },
+
 
   // project: {
   //   link: "https://www.linkedin.com/in/jayjeet-kumar-71a7751ba/",
@@ -25,6 +45,13 @@ const config: DocsThemeConfig = {
   //   link: "https://twitter.com/Jayjeet_100",
   //   icon: <TwitterIcon style={{ marginLeft: '-12px' }} />,
   // },
+
+  // banner: {
+  //   text: "It is in Beta  ",
+  //   dismissible: false,
+  //   key: "4",
+  // },
+
   sidebar: {
     autoCollapse: false,
     defaultMenuCollapseLevel: 1,
@@ -36,7 +63,7 @@ const config: DocsThemeConfig = {
   },
   useNextSeoProps() {
     return {
-      titleTemplate: "UI library",
+      titleTemplate: "Aura UI",
     };
   },
 };
