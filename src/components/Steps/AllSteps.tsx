662b0746a1d9bcce1dcde124e1657853e545d978
Jayjeet1013 <jayjeet1013@gmail.com>
2024-05-22 13:13:03 +0530

    done steps component

--- src/components/Steps/AllSteps.tsx
+++ src/components/Steps/AllSteps.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import VerticleSteps from "./VerticleSteps";
 
-type Props = {}
+const AllSteps = () => {
+  return (
+    <div className="space-y-10 pt-10 ">
+      <div className=" ">
+        <div className="flex my-2 justify-between">
+          <h3 className="text-[20px] font-medium ">VerticalSteps Component </h3>
+          <Link href={"/components/Steps/Elements#verticlesteps-component"}>
+            <button className="py-1 px-2 font-semibold  rounded-3xl text-[14px] bg-white text-blue-600">
+              Get Code
+            </button>
+          </Link>
+        </div>
+        <VerticleSteps/>
+      </div>
+    </div>
+  );
+};
 
-function AllSteps({}: Props) {
-	return (
-		<div>AllSteps</div>
-	)
-}
-
-export default AllSteps
+export default AllSteps;
---
+++ src/pages/preview/steps/vertical.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import VerticleSteps from "@/components/Steps/VerticleSteps";
+
+type Props = {};
+
+function Verticle({}: Props) {
+  return <VerticleSteps />;
+}
+
+export default Verticle;
