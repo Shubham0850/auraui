8b512e70137d89763804661b366c990e8f8a7c9c
Jayjeet1013 <jayjeet1013@gmail.com>
2024-05-22 19:56:10 +0530

    done walletconnect

---
+++ src/pages/preview/web3/walletconnect/walletlinker.tsx
@@ -0,0 +1,11 @@
+import WalletLinker from "@/components/Web3/WalletConnect/WalletLinker";
+import React from "react";
+
+
+type Props = {};
+
+function Walletlinker({}: Props) {
+  return <WalletLinker/>;
+}
+
+export default Walletlinker;
