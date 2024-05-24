83e583a726e82bb53c3ad39f90f8299809eb2859
Jayjeet1013 <jayjeet1013@gmail.com>
2024-05-24 22:08:25 +0530

    done footer

--- src/components/Footer/AllFooter.tsx
+++ src/components/Footer/AllFooter.tsx
@@ -1,36 +1,23 @@
 import React from "react";
 import AuraFooterOne from "./AuraFooterOne";
 import SmallFooter from "./SmallFooter";
-import Link from "next/link";
+import ComponentWrapper from "../Theme/ComponentWrapper";
 
 const AllFooter = () => {
   return (
-    <div className="space-y-10 pt-10 ">
-      <div className=" ">
-        <div className="flex my-2 justify-between">
-          <h3 className="text-[20px] font-medium ">AuraFooter Component </h3>
-          <Link href={"/components/footer/Elements#aurafooter-component"}>
-            <button className="py-1 px-2 font-semibold  rounded-3xl text-[14px] bg-white text-blue-600">
-              Get Code
-            </button>
-          </Link>
-        </div>
+    <div>
+      <ComponentWrapper
+        title="Aura Footer"
+        previewLink="/components/footer/Elements#aurafooter-component"
+      >
         <AuraFooterOne />
-      </div>
-      <div className=" ">
-        <div className="flex my-2 justify-between">
-          <h3 className="text-[20px] font-medium ">Small Footer Component </h3>
-          <Link
-            className=" "
-            href={"/components/footer/Elements#small-footer-component"}
-          >
-            <button className="py-1 px-2 font-semibold  rounded-3xl text-[14px] bg-white text-blue-600">
-              Get Code
-            </button>
-          </Link>
-        </div>
+      </ComponentWrapper>
+      <ComponentWrapper
+        title="Small Footer "
+        previewLink="/components/footer/Elements#small-footer-component"
+      >
         <SmallFooter />
-      </div>
+      </ComponentWrapper>
     </div>
   );
 };
