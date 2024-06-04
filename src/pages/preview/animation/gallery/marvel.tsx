fa3e4a782950e59cf1aca2a3a796cfb99166b68a
Shubham0850 <rajshubham0850@gmail.com>
2024-06-04 17:28:08 +0530

    2 animated component added

---
+++ src/pages/preview/animation/gallery/marvel.tsx
@@ -0,0 +1,12 @@
+import MarvelGallery from '@/components/animation/MarvelGallery'
+import React from 'react'
+
+type Props = {}
+
+function Page({}: Props) {
+	return (
+		<MarvelGallery/>
+	)
+}
+
+export default Page
---
+++ src/pages/preview/animation/gallery/music.tsx
@@ -0,0 +1,12 @@
+import GalleryMusic from '@/components/animation/MusicGallery'
+import React from 'react'
+
+type Props = {}
+
+function Page({}: Props) {
+	return (
+		<GalleryMusic/>
+	)
+}
+
+export default Page
