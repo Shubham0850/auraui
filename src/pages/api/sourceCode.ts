85e3bdea7c1bd9633adc033be5338d604181973e
Jayjeet1013 <jayjeet1013@gmail.com>
2024-06-16 17:22:06 +0530

    add console.log

--- src/pages/api/sourceCode.ts
+++ src/pages/api/sourceCode.ts
@@ -1,24 +1,28 @@
 // pages/api/sourceCode.js
-import fs from 'fs';
-import { NextApiRequest, NextApiResponse } from 'next';
-import path from 'path';
+import fs from "fs";
+import { NextApiRequest, NextApiResponse } from "next";
+import path from "path";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const componentPath = Array.isArray(req.query.componentPath) ? req.query.componentPath[0] : req.query.componentPath;
+  const componentPath = Array.isArray(req.query.componentPath)
+    ? req.query.componentPath[0]
+    : req.query.componentPath;
 
   if (!componentPath) {
-    res.status(400).json({ error: 'Component path is required' });
+    res.status(400).json({ error: "Component path is required" });
     return;
   }
 
-  console.log({ componentPath })
+  console.log({ componentPath });
 
   const filePath = path.join(process.cwd(), `/src/components/${componentPath}`);
-
+  console.log(filePath);
   try {
-    const sourceCode = fs.readFileSync(filePath, 'utf-8');
+    const sourceCode = fs.readFileSync(filePath, "utf-8");
     res.status(200).json({ sourceCode });
+    console.log(sourceCode);
   } catch (error) {
-    res.status(500).json({ error: 'Error fetching source code' });
+    console.log(error);
+    res.status(500).json({ error: "Error fetching source code" });
   }
 }
