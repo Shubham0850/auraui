dec55849ad266dfac69f3c0ce8279c61bab0d0d7
Jayjeet1013 <jayjeet1013@gmail.com>
2024-06-17 19:19:36 +0530

    update previous api

--- src/pages/api/sourceCode.ts
+++ src/pages/api/sourceCode.ts
@@ -1,4 +1,4 @@
-// pages/api/sourceCode.ts
+// pages/api/sourceCode.js
 import fs from "fs";
 import { NextApiRequest, NextApiResponse } from "next";
 import path from "path";
@@ -15,42 +15,17 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 
   console.log({ componentPath });
 
-  // Construct the file path
-  const filePath = path.join(process.cwd(), "src", "components", componentPath);
-  console.log(`process.cwd(): ${process.cwd()}`);
-  console.log(`Resolved file path: ${filePath}`);
-
-  // Function to list files in the directory
-  function listFilesInDirectory(directoryPath: string) {
-    try {
-      const files = fs.readdirSync(directoryPath);
-      console.log(`Files in ${directoryPath}:`, files);
-    } catch (error) {
-      console.error(`Error reading directory: ${directoryPath}`, error);
-    }
-  }
-
-  // Call this function to list files in the relevant directories step-by-step
-  listFilesInDirectory(path.join(process.cwd(), "src"));
-  listFilesInDirectory(path.join(process.cwd(), "src", "components"));
-  listFilesInDirectory(
-    path.join(process.cwd(), "src", "components", path.dirname(componentPath))
+  const filePath = path.join(
+    process.cwd(),
+    ` /src/components/${componentPath} `
   );
-
+  console.log({ filePath });
   try {
-    // Check if the file exists
-    if (!fs.existsSync(filePath)) {
-      console.error(`File not found: ${filePath}`);
-      res.status(404).json({ error: "File not found" });
-      return;
-    }
-
-    // Read the file content
     const sourceCode = fs.readFileSync(filePath, "utf-8");
     res.status(200).json({ sourceCode });
     console.log({ sourceCode });
   } catch (error) {
-    console.error(`Error reading file: ${filePath}`, error);
+    console.log({ error });
     res.status(500).json({ error: "Error fetching source code" });
   }
 }
