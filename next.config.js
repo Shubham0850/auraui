9635d3d72e84d1e7b878e874447487a6e1115a3e
Jayjeet1013 <jayjeet1013@gmail.com>
2024-07-14 22:33:37 +0530

    done contact

--- next.config.js
+++ next.config.js
@@ -14,16 +14,22 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'i.pravatar.cc',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.auraui.com",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
