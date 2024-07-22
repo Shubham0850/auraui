641a0457882c6910f9b19d63964eea93dfc0b58e
Kishor <yoyokunal156@gmail.com>
2024-07-22 16:56:01 +0530

    Enhanced Loader

--- src/components/Theme/SkeletonLoader.tsx
+++ src/components/Theme/SkeletonLoader.tsx
@@ -1,12 +1,11 @@
-// components/SkeletonLoader.js
-import Skeleton from "react-loading-skeleton";
+import React from "react";
 
-const SkeletonLoader = () => {
+const SourceCodeSkeleton: React.FC = () => {
   return (
-    <div>
-      <Skeleton className="h-[490px] skeleton" />
+    <div className="relative h-[490px] overflow-y-auto rounded-lg custom-scrollbar mt-1 animate-pulse">
+      <div className="bg-gray-700 w-full h-full"></div>
     </div>
   );
 };
 
-export default SkeletonLoader;
+export default SourceCodeSkeleton;
--- src/utils/SourceCodeViewer.tsx
+++ src/utils/SourceCodeViewer.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useEffect, useState } from "react";
 import { fetchSourceCode } from "./getSourceCode";
 import { CopyToClipboard } from "nextra/components";
@@ -10,7 +12,6 @@ function SourceCodeViewer({ componentPath }: { componentPath: string }) {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setLoading(true); // Set loading to true when component path changes
     fetchSourceCode(componentPath)
       .then((code) => {
         setSourceCode(code);
@@ -26,20 +27,21 @@ function SourceCodeViewer({ componentPath }: { componentPath: string }) {
 
   return (
     <pre className="relative h-[500px] overflow-y-auto custom-scrollbar p-0 m-0">
-      <div className="">
-        <div className="bg-transparent z-20 sticky float-right -mb-4 top-2">
-          <CopyToClipboard getValue={getValue} />
-        </div>
-        {loading ? (
-          <SkeletonLoader />
-        ) : (
+      {loading ? (
+        <SkeletonLoader />
+      ) : (
+        <div className="">
+          <div className="bg-transparent z-20 sticky float-right -mb-4 top-2">
+            <CopyToClipboard getValue={getValue} />
+          </div>
+
           <code>
             <SyntaxHighlighter language="jsx" style={vscDarkPlus} wrapLines>
               {sourceCode}
             </SyntaxHighlighter>
           </code>
-        )}
-      </div>
+        </div>
+      )}
     </pre>
   );
 }
