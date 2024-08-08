ae146fca17f4e7a94326eaf6a719cd5c8fd44777
Kishor <yoyokunal156@gmail.com>
2024-08-08 17:37:00 +0530

    testimonial7

--- src/components/Testimonial/Testimonial7.tsx
+++ src/components/Testimonial/Testimonial7.tsx
@@ -1,103 +1,99 @@
 import React from "react";
 
+const commonClasses = {
+  textWhite: "text-white",
+  blockQuoteText: "font-serif text-2xl italic leading-tight xl:text-4xl",
+  textOpacity: "text-opacity-50",
+  textBase: "text-base font-normal",
+  gradientBg: "bg-gradient-to-t from-secondary to-transparent",
+
+  border: "border rounded-lg border-neutral",
+  container: "relative overflow-hidden h-[400px] w-[300px]",
+  sectionHeading: "text-4xl font-normal sm:text-5xl md:text-6xl",
+  sectionSubHeading: "text-5xl italic sm:text-6xl md:text-7xl",
+};
+
 function Testimonial7() {
   return (
     <section className="py-12 sm:py-16 lg:py-20 bg-black">
       <div className="px-6 mx-auto sm:px-8 lg:px-12 max-w-7xl">
         <div className="grid grid-cols-1 text-center gap-y-8 lg:text-left lg:grid-cols-6">
           <div className="lg:col-span-4">
-            <h2 className="tracking-tighter text-white">
-              <span className="font-sans text-4xl font-normal sm:text-5xl md:text-6xl">
-                {" "}
-                Recommended{" "}
+            <h2 className={`tracking-tighter ${commonClasses.textWhite}`}>
+              <span className={`font-sans ${commonClasses.sectionHeading}`}>
+                Recommended
               </span>
-              <span className="block font-serif text-5xl italic sm:text-6xl md:text-7xl">
-                {" "}
-                by our members{" "}
+              <span
+                className={`block font-serif ${commonClasses.sectionSubHeading}`}
+              >
+                by our members
               </span>
             </h2>
           </div>
 
           <div className="lg:col-span-2">
-            <p className="font-sans text-lg font-normal leading-8 text-opacity-50 text-white">
-              Urna, in est malesuada arcu massa posuere convallis. Id tristique
-              sed velit tellus lorem pellentesque id ut ultrices.
+            <p
+              className={`font-sans text-lg font-normal leading-8 ${commonClasses.textOpacity} ${commonClasses.textWhite}`}
+            >
+              AuraUI provides an exceptional experience, enhancing design
+              workflows and achieving greater efficiency in development.
             </p>
           </div>
         </div>
 
         <div className="grid max-w-md grid-cols-1 gap-5 mx-auto mt-12 lg:grid-cols-3 lg:mt-16 lg:max-w-none">
-          <div className="relative overflow-hidden border rounded-lg border-neutral">
-            <div className="aspect-w-3 aspect-h-4">
-              <img
-                className="object-cover w-full h-full"
-                src="https://cdn.rareblocks.xyz/collection/bakerstreet/images/testimonials/5/member-1.png"
-                alt=""
-              />
-            </div>
-            <div className="absolute inset-0 bg-gradient-to-t from-secondary to-transparent"></div>
-            <div className="absolute inset-x-0 bottom-0">
-              <div className="px-8 pb-6">
-                <blockquote>
-                  <p className="font-serif text-2xl italic leading-tight xl:text-4xl text-white">
-                    “Sed vitae egestas maecenas arcu ultrices ipsum suspendisse
-                    amet consequat.”
+          {[
+            {
+              name: "Cymone Magdalina",
+              image: "https://www.auraui.com/memeimage/woman-potrait.webp",
+              quote:
+                "“AuraUI’s design tools have significantly improved our project turnaround time, making our workflow more efficient.”",
+            },
+            {
+              name: "Sophia Ella",
+              image: "https://www.auraui.com/memeimage/woman-potrait-3.jpeg",
+              quote:
+                "“With AuraUI, our design team can quickly prototype and deploy high-quality components with ease.”",
+            },
+            {
+              name: "Hyeon Vivek",
+              image: "https://www.auraui.com/memeimage/man-potrait.jpg",
+              quote:
+                "“The versatility of AuraUI’s components has revolutionized our development process and enhanced our end products.”",
+            },
+          ].map(({ name, image, quote }, index) => (
+            <div
+              key={index}
+              className={`${commonClasses.container} ${commonClasses.border}`}
+            >
+              <div className="aspect-w-3 aspect-h-4">
+                <img
+                  className="object-fill h-full w-full"
+                  src={image}
+                  alt={`Testimonial from ${name}`}
+                />
+              </div>
+              <div
+                className={`${commonClasses.gradientBg} absolute inset-0`}
+              ></div>
+              <div className="absolute inset-x-0 bottom-0">
+                <div className="px-8 pb-6">
+                  <blockquote>
+                    <p
+                      className={`${commonClasses.blockQuoteText} ${commonClasses.textWhite}`}
+                    >
+                      {quote}
+                    </p>
+                  </blockquote>
+                  <p
+                    className={`mt-6 ${commonClasses.textBase} xl:mt-8 ${commonClasses.textWhite}`}
+                  >
+                    {name}
                   </p>
-                </blockquote>
-                <p className="mt-6 font-sans text-base font-normal xl:mt-8 text-white">
-                  Cymone Magdalina
-                </p>
+                </div>
               </div>
             </div>
-          </div>
-
-          <div className="relative overflow-hidden border rounded-lg border-neutral">
-            <div className="aspect-w-3 aspect-h-4">
-              <img
-                className="object-cover w-full h-full"
-                src="https://cdn.rareblocks.xyz/collection/bakerstreet/images/testimonials/5/member-2.png"
-                alt=""
-              />
-            </div>
-            <div className="absolute inset-0 bg-gradient-to-t from-secondary to-transparent"></div>
-            <div className="absolute inset-x-0 bottom-0">
-              <div className="px-8 pb-6">
-                <blockquote>
-                  <p className="font-serif text-2xl italic leading-tight xl:text-4xl text-white">
-                    “Sed vitae egestas maecenas arcu ultrices ipsum suspendisse
-                    amet consequat.”
-                  </p>
-                </blockquote>
-                <p className="mt-6 font-sans text-base font-normal xl:mt-8 text-white">
-                  Sophia Ella
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div className="relative overflow-hidden border rounded-lg border-neutral">
-            <div className="aspect-w-3 aspect-h-4">
-              <img
-                className="object-cover w-full h-full"
-                src="https://cdn.rareblocks.xyz/collection/bakerstreet/images/testimonials/5/member-3.png"
-                alt=""
-              />
-            </div>
-            <div className="absolute inset-0 bg-gradient-to-t from-secondary to-transparent"></div>
-            <div className="absolute inset-x-0 bottom-0">
-              <div className="px-8 pb-6">
-                <blockquote>
-                  <p className="font-serif text-2xl italic leading-tight xl:text-4xl text-white">
-                    “Sed vitae egestas maecenas arcu ultrices ipsum suspendisse
-                    amet consequat.”
-                  </p>
-                </blockquote>
-                <p className="mt-6 font-sans text-base font-normal xl:mt-8 text-white">
-                  Hyeon Vivek
-                </p>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
---
+++ src/pages/preview/testimonial/testimonial7.tsx
@@ -0,0 +1,10 @@
+import Testimonial7 from "@/components/Testimonial/Testimonial7";
+import React from "react";
+
+type Props = {};
+
+function testimonial7({}: Props) {
+  return <Testimonial7 />;
+}
+
+export default testimonial7;
