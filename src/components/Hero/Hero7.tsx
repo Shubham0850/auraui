1f5ee3dc574a55dfb130676ce19c349dc7eab8fc
Kishor <yoyokunal156@gmail.com>
2024-08-17 03:35:05 +0530

    hero7

--- src/components/Hero/Hero7.tsx
+++ src/components/Hero/Hero7.tsx
@@ -1,29 +1,33 @@
 import React from "react";
 
 function Hero7() {
+  const commonStyles = {
+    button:
+      "inline-flex items-center justify-center px-5 py-2 font-sans text-base font-semibold transition-all duration-200 rounded-full sm:leading-8 sm:text-lg focus:outline-none focus:ring-2 focus:ring-offset-2",
+    focusRing: "focus:ring-primary focus:ring-offset-secondary",
+  };
+
   return (
     <div className="relative pt-48 pb-12 bg-black xl:pt-60 sm:pb-16 lg:pb-32 xl:pb-48 2xl:pb-56">
       <header className="absolute inset-x-0 top-0 z-10 py-8 xl:py-12">
         <div className="px-6 mx-auto sm:px-8 lg:px-12 max-w-7xl">
           <div className="flex items-center justify-between">
-            <div className="flex flex-shrink-0">
-              <a
-                href="#"
-                title="BakerStreet"
-                className="inline-flex rounded-md focus:outline-none focus:ring-2 focus:ring-offset-4 focus:ring-offset-secondary focus:ring-primary"
-              >
-                <img
-                  className="w-auto h-8"
-                  src="https://cdn.rareblocks.xyz/collection/bakerstreet/images/logo.svg"
-                  alt="BakerStreet"
-                />
-              </a>
-            </div>
+            <a
+              href="#"
+              title="AuraUI"
+              className="inline-flex rounded-md focus:outline-none focus:ring-2 focus:ring-offset-4 focus:ring-offset-secondary focus:ring-primary"
+            >
+              <img
+                className="w-auto h-8"
+                src="https://www.auraui.com/logo-dark.png"
+                alt="AuraUI Logo"
+              />
+            </a>
 
             <div className="md:hidden">
               <button
                 type="button"
-                className="p-2 -m-2 transition-all duration-200 rounded-full text-white focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-primary focus:ring-offset-secondary"
+                className="p-2 -m-2 text-white rounded-full focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-primary focus:ring-offset-secondary"
               >
                 <svg
                   className="w-6 h-6"
@@ -33,52 +37,35 @@ function Hero7() {
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M4 6h16M4 12h16M4 18h16"
                   />
                 </svg>
               </button>
             </div>
 
-            <div className="hidden md:flex md:items-center md:space-x-10 lg:ml-28">
-              <a
-                href="#"
-                title=""
-                className="font-sans text-base font-normal transition-all duration-200 rounded text-white focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-primary focus:ring-offset-secondary"
-              >
-                {" "}
-                About{" "}
-              </a>
-
-              <a
-                href="#"
-                title=""
-                className="font-sans text-base font-normal transition-all duration-200 rounded text-white focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-primary focus:ring-offset-secondary"
-              >
-                {" "}
-                Services{" "}
-              </a>
-
-              <a
-                href="#"
-                title=""
-                className="font-sans text-base font-normal transition-all duration-200 rounded text-white focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-primary focus:ring-offset-secondary"
-              >
-                {" "}
-                Contacts{" "}
-              </a>
-            </div>
+            <nav className="hidden md:flex md:items-center md:space-x-10 lg:ml-28">
+              {["About", "Services", "Contact"].map((item) => (
+                <a
+                  key={item}
+                  href="#"
+                  className="font-sans text-base font-normal text-white transition-all duration-200 rounded focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-primary focus:ring-offset-secondary"
+                >
+                  {item}
+                </a>
+              ))}
+            </nav>
           </div>
         </div>
       </header>
 
       <div className="absolute inset-0">
         <img
-          className="object-cover w-full h-full"
-          src="https://cdn.rareblocks.xyz/collection/bakerstreet/images/hero/2/background.png"
-          alt=""
+          className="object-cover w-full h-full opacity-60"
+          src="/memeimage/working-women-3.jpg"
+          alt="AuraUI Background"
         />
       </div>
 
@@ -86,75 +73,27 @@ function Hero7() {
         <div className="px-6 mx-auto sm:px-8 lg:px-12 max-w-7xl">
           <div className="max-w-xl mx-auto text-center">
             <h1 className="tracking-tighter text-white">
-              <span className="font-sans font-normal text-7xl">Master</span>
+              <span className="font-sans font-normal text-7xl">Discover</span>
               <br />
               <span className="font-serif italic font-normal text-8xl">
-                Bread making
+                AuraUI
               </span>
             </h1>
             <p className="mt-5 font-sans text-base font-normal text-white text-opacity-70">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu
-              penatibus pellentesque dolor consequat ligula egestas massa
-              gravida. Porttitor venenatis enim praesent.
+              Building seamless UI experiences with AuraUI components. Enhance
+              your projects effortlessly with our high-performance solutions.
             </p>
             <div className="flex items-center justify-center mt-8 space-x-3 sm:space-x-4">
               <a
                 href="#"
-                title=""
-                className="
-                            inline-flex
-                            items-center
-                            justify-center
-                            px-5
-                            py-2
-                            font-sans
-                            text-base
-                            font-semibold
-                            leading-6
-                            transition-all
-                            duration-200
-                            border-2 border-transparent
-                            rounded-full
-                            sm:leading-8
-                            bg-white
-                            sm:text-lg
-                            text-black
-                            hover:bg-opacity-90
-                            focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary focus:ring-offset-secondary
-                        "
-                role="button"
+                className={`${commonStyles.button} bg-white text-black hover:bg-opacity-90 ${commonStyles.focusRing} border-2 border-transparent`}
               >
                 Get started
               </a>
 
               <a
                 href="#"
-                title=""
-                className="
-                            inline-flex
-                            items-center
-                            justify-center
-                            px-5
-                            py-2
-                            font-sans
-                            text-base
-                            font-semibold
-                            leading-6
-                            transition-all
-                            duration-200
-                            bg-transparent
-                            border-2
-                            rounded-full
-                            sm:leading-8
-                            text-white
-                            border-primary
-                            hover:bg-white
-                            focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary
-                            hover:text-black
-                            sm:text-lg
-                            focus:ring-offset-secondary
-                        "
-                role="button"
+                className={`${commonStyles.button} bg-transparent text-white border-primary hover:bg-white hover:text-black ${commonStyles.focusRing} border-2`}
               >
                 <svg
                   className="w-6 h-6 mr-2"
@@ -163,8 +102,8 @@ function Hero7() {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
                     d="M8.0416 4.9192C7.37507 4.51928 6.5271 4.99939 6.5271 5.77669L6.5271 18.2232C6.5271 19.0005 7.37507 19.4806 8.0416 19.0807L18.4137 12.8574C19.061 12.469 19.061 11.5308 18.4137 11.1424L8.0416 4.9192Z"
                   />
                 </svg>
@@ -178,9 +117,7 @@ function Hero7() {
       <div className="absolute hidden transform -translate-x-1/2 lg:bottom-8 xl:bottom-12 left-1/2 lg:block">
         <a
           href="#"
-          title=""
-          className="inline-flex items-center justify-center w-12 h-12 transition-all duration-200 rounded-full text-white hover:bg-white hover:text-black bg-black focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-primary focus:ring-offset-secondary"
-          role="button"
+          className="inline-flex items-center justify-center w-12 h-12 text-white transition-all duration-200 rounded-full bg-black hover:bg-white hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-primary focus:ring-offset-secondary"
         >
           <svg
             className="w-5 h-5"
@@ -190,9 +127,9 @@ function Hero7() {
             stroke="currentColor"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M19 9l-7 7-7-7"
             />
           </svg>
