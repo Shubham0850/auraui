f03dd363d73c00b80399cf273bb25d6138390a65
Kishor <yoyokunal156@gmail.com>
2024-08-22 19:00:33 +0530

    Hero 22

--- src/components/Hero/Hero22.tsx
+++ src/components/Hero/Hero22.tsx
@@ -1,4 +1,27 @@
 import React from "react";
+import { FaDownload } from "react-icons/fa";
+import { HiOutlineBars3 } from "react-icons/hi2";
+
+const commonStyles = {
+  header: "relative py-4 md:py-6",
+  container: "px-4 mx-auto max-w-7xl sm:px-6 lg:px-8",
+  logo: "flex rounded outline-none focus:ring-1 focus:ring-gray-900 focus:ring-offset-2",
+  menuIcon: "w-7 h-7",
+  navLink:
+    "text-base font-medium text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2",
+  button:
+    "inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-white transition-all duration-200 border border-transparent rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 font-pj",
+  primaryButton: "bg-gray-900 hover:bg-gray-600",
+  secondaryButton:
+    "bg-transparent border-gray-900 hover:bg-gray-900 hover:text-white",
+  sectionTitle:
+    "text-4xl font-bold leading-tight text-gray-900 sm:text-5xl sm:leading-tight lg:text-6xl lg:leading-tight font-pj",
+  textAccent: "relative inline-flex sm:inline",
+  textHighlight:
+    "bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] blur-lg filter opacity-30 w-full h-full absolute inset-0",
+  profilePic: "inline-block rounded-full w-14 h-14 ring-2 ring-white",
+  gridImg: "w-full mx-auto scale-110",
+};
 
 function Hero22() {
   return (
@@ -6,92 +29,53 @@ function Hero22() {
       <div className="absolute bottom-0 right-0 overflow-hidden lg:inset-y-0">
         <img
           className="w-auto h-full"
-          src="https://d33wubrfki0l68.cloudfront.net/1e0fc04f38f5896d10ff66824a62e466839567f8/699b5/images/hero/3/background-pattern.png"
-          alt=""
+          src="https://www.auraui.com/memeimage/background-pattern.png"
+          alt="Background Pattern"
         />
       </div>
 
-      <header className="relative py-4 md:py-6">
-        <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+      <header className={commonStyles.header}>
+        <div className={commonStyles.container}>
           <div className="flex items-center justify-between">
             <div className="flex-shrink-0">
-              <a
-                href="#"
-                title=""
-                className="flex rounded outline-none focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
-              >
+              <a href="#" title="AuraUI Home" className={commonStyles.logo}>
                 <img
                   className="w-auto h-8"
-                  src="https://d33wubrfki0l68.cloudfront.net/682a555ec15382f2c6e7457ca1ef48d8dbb179ac/f8cd3/images/logo.svg"
-                  alt=""
+                  src="https://www.auraui.com/logo-light.png"
+                  alt="AuraUI Logo"
                 />
               </a>
             </div>
 
             <div className="flex lg:hidden">
               <button type="button" className="text-gray-900">
-                <svg
-                  className="w-7 h-7"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="1.5"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  ></path>
-                </svg>
+                <HiOutlineBars3 className={commonStyles.menuIcon} />
               </button>
             </div>
 
             <div className="hidden lg:flex lg:ml-16 lg:items-center lg:justify-center lg:space-x-10">
               <div className="flex items-center space-x-12">
-                <a
-                  href="#"
-                  title=""
-                  className="text-base font-medium text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
-                >
-                  {" "}
-                  Features{" "}
+                <a href="#" title="Features" className={commonStyles.navLink}>
+                  Features
                 </a>
-
-                <a
-                  href="#"
-                  title=""
-                  className="text-base font-medium text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
-                >
-                  {" "}
-                  Pricing{" "}
+                <a href="#" title="Pricing" className={commonStyles.navLink}>
+                  Pricing
                 </a>
-
-                <a
-                  href="#"
-                  title=""
-                  className="text-base font-medium text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
-                >
-                  {" "}
-                  Automation{" "}
+                <a href="#" title="Automation" className={commonStyles.navLink}>
+                  Automation
                 </a>
               </div>
 
               <div className="w-px h-5 bg-gray-300"></div>
 
-              <a
-                href="#"
-                title=""
-                className="text-base font-medium text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
-              >
-                {" "}
-                Login{" "}
+              <a href="#" title="Login" className={commonStyles.navLink}>
+                Login
               </a>
 
               <a
                 href="#"
-                title=""
-                className="px-5 py-2 text-base font-semibold leading-7 text-gray-900 transition-all duration-200 bg-transparent border border-gray-900 rounded-xl font-pj focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 hover:bg-gray-900 hover:text-white focus:bg-gray-900 focus:text-white"
+                title="Create Free Account"
+                className={`${commonStyles.button} ${commonStyles.primaryButton}`}
                 role="button"
               >
                 Create free account
@@ -102,37 +86,37 @@ function Hero22() {
       </header>
 
       <section className="relative py-12 sm:py-16 lg:pt-20 lg:pb-36">
-        <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+        <div className={commonStyles.container}>
           <div className="grid grid-cols-1 gap-y-8 lg:items-center lg:grid-cols-2 sm:gap-y-20 xl:grid-cols-5">
             <div className="text-center xl:col-span-2 lg:text-left md:px-16 lg:px-0">
               <div className="max-w-sm mx-auto sm:max-w-md md:max-w-full">
-                <h1 className="text-4xl font-bold leading-tight text-gray-900 sm:text-5xl sm:leading-tight lg:text-6xl lg:leading-tight font-pj">
-                  Get meaningful feedbacks on your code
+                <h1 className={commonStyles.sectionTitle}>
+                  Get meaningful feedback on your code
                 </h1>
 
                 <div className="mt-8 lg:mt-12 lg:flex lg:items-center">
                   <div className="flex justify-center flex-shrink-0 -space-x-4 overflow-hidden lg:justify-start">
                     <img
-                      className="inline-block rounded-full w-14 h-14 ring-2 ring-white"
-                      src="https://d33wubrfki0l68.cloudfront.net/3bfa6da479d6b9188c58f2d9a8d33350290ee2ef/301f1/images/hero/3/avatar-male.png"
-                      alt=""
+                      className={commonStyles.profilePic}
+                      src="https://www.auraui.com/memeimage/woman1.jpg"
+                      alt="Profile"
                     />
                     <img
-                      className="inline-block rounded-full w-14 h-14 ring-2 ring-white"
-                      src="https://d33wubrfki0l68.cloudfront.net/b52fa09a115db3a80ceb2d52c275fadbf84cf8fc/7fd8a/images/hero/3/avatar-female-1.png"
-                      alt=""
+                      className={commonStyles.profilePic}
+                      src="https://www.auraui.com/memeimage/man2.jpg"
+                      alt="Profile"
                     />
                     <img
-                      className="inline-block rounded-full w-14 h-14 ring-2 ring-white"
-                      src="https://d33wubrfki0l68.cloudfront.net/8a2efb13f103a5ae2909e244380d73087a9c2fc4/31ed6/images/hero/3/avatar-female-2.png"
-                      alt=""
+                      className={commonStyles.profilePic}
+                      src="https://www.auraui.com/memeimage/woman3.jpg"
+                      alt="Profile"
                     />
                   </div>
 
                   <p className="mt-4 text-lg text-gray-900 lg:mt-0 lg:ml-4 font-pj">
                     Join with{" "}
                     <span className="font-bold">4600+ Developers</span> and
-                    start getting feedbacks right now
+                    start getting feedback now
                   </p>
                 </div>
               </div>
@@ -140,8 +124,8 @@ function Hero22() {
               <div className="mt-8 sm:flex sm:items-center sm:justify-center lg:justify-start sm:space-x-5 lg:mt-12">
                 <a
                   href="#"
-                  title=""
-                  className="inline-flex items-center px-8 py-4 text-lg font-bold text-white transition-all duration-200 bg-gray-900 border border-transparent rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 font-pj justif-center hover:bg-gray-600"
+                  title="Get Feedback"
+                  className={`${commonStyles.button} ${commonStyles.primaryButton}`}
                   role="button"
                 >
                   Get feedback
@@ -149,24 +133,11 @@ function Hero22() {
 
                 <a
                   href="#"
-                  title=""
-                  className="inline-flex items-center px-4 py-4 mt-4 text-lg font-bold transition-all duration-200 bg-transparent border border-transparent sm:mt-0 font-pj justif-center rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 hover:bg-gray-200 focus:bg-gray-200"
+                  title="Download iOS App"
+                  className={`${commonStyles.button} ${commonStyles.secondaryButton}`}
                   role="button"
                 >
-                  <svg
-                    className="w-5 h-5 mr-2"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2.5"
-                      d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
-                    ></path>
-                  </svg>
+                  <FaDownload className="w-5 h-5 text-gray-900 mr-2" />
                   Download iOS App
                 </a>
               </div>
@@ -174,9 +145,9 @@ function Hero22() {
 
             <div className="xl:col-span-3">
               <img
-                className="w-full mx-auto scale-110"
-                src="https://d33wubrfki0l68.cloudfront.net/29c501c64b21014b3f2e225abe02fe31fd8f3a5c/f866d/images/hero/3/illustration.png"
-                alt=""
+                className={commonStyles.gridImg}
+                src="https://www.auraui.com/memeimage/hero22.png"
+                alt="Hero Illustration"
               />
             </div>
           </div>
