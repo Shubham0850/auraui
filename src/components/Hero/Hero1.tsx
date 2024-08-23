6597f8709975bae15da88d545a0bebb982f2c781
Kishor <yoyokunal156@gmail.com>
2024-08-23 11:49:27 +0530

    Hero 1 images

--- src/components/Hero/Hero1.tsx
+++ src/components/Hero/Hero1.tsx
@@ -19,7 +19,7 @@ function Hero1() {
       <div className="absolute inset-0">
         <img
           className="object-cover w-full h-full"
-          src="https://images.unsplash.com/photo-1699891730676-037bed3c1bed?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          src="/memeimage/hero1.jpg"
           alt=""
         />
       </div>
