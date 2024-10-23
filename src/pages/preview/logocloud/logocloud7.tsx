d9e193c2ee28341374157090f117a94e53a719b7
Jayjeet1013 <jayjeet1013@gmail.com>
2024-10-23 12:51:20 +0530

    update logocloud doc

---
+++ src/pages/preview/logocloud/logocloud7.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import LogoCloud7 from "@/components/LogoCloud/LogoCloud7";
+
+type Props = {};
+
+function LogoCloudPage({}: Props) {
+  return <LogoCloud7 />;
+}
+
+export default LogoCloudPage;
---
+++ src/pages/preview/logocloud/logocloud8.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import LogoCloud8 from "@/components/LogoCloud/LogoCloud8";
+
+type Props = {};
+
+function LogoCloudPage({}: Props) {
+  return <LogoCloud8 />;
+}
+
+export default LogoCloudPage;
